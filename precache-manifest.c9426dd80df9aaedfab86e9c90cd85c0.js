72f36db299331e91739ab8a5397ca06a1eb2f905
jbmerand <jbmerand@icloud.com>
2019-01-31 21:03:03 +0100

    Updates

---
+++ precache-manifest.c9426dd80df9aaedfab86e9c90cd85c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "89492bf9d0074e984e12",
+    "url": "/reactjs-face-recognition-front-end/static/js/runtime~main.89492bf9.js"
+  },
+  {
+    "revision": "a1956797c80f50ba7d09",
+    "url": "/reactjs-face-recognition-front-end/static/js/main.a1956797.chunk.js"
+  },
+  {
+    "revision": "b560a510dd1b715bdc95",
+    "url": "/reactjs-face-recognition-front-end/static/js/1.b560a510.chunk.js"
+  },
+  {
+    "revision": "a1956797c80f50ba7d09",
+    "url": "/reactjs-face-recognition-front-end/static/css/main.16c1581c.chunk.css"
+  },
+  {
+    "revision": "b560a510dd1b715bdc95",
+    "url": "/reactjs-face-recognition-front-end/static/css/1.75da082f.chunk.css"
+  },
+  {
+    "revision": "30c24593ccdf903434828acecda6e31a",
+    "url": "/reactjs-face-recognition-front-end/index.html"
+  }
+];
